8b8bd54f0334284454ee5466a02c3f1862d0ccbb
Deluxe Wolf <57351713+deluxewolf@users.noreply.github.com>
2022-09-15 11:36:08 +0100

    site

---
+++ assets/config.js
@@ -0,0 +1,22 @@
+let config = {
+  "Title": "Shuaib Hayat - Links",
+  "Name": "Shuaib Hayat",
+  "Description": "Software Developer / BSc (Hons) Computer Science Graduate",
+  "links": [
+    {
+      "Title": "Website",
+      "URL": "http://shuaibhayat.tk",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Github (public)",
+      "URL": "https://github.com/deluxewolf",
+      "icon_classes": "fab fa-github",
+    },
+    {
+      "Title": "Contact via Email (shuaib.hayat@outlook.com)",
+      "URL": "mailto:shuaib.hayat@outlook.com",
+      "icon_classes": "fas fa-envelope"
+    },
+  ]
+}
